8b99e33dad52859dea3c35acd6b0bfb2394c7b29
mafro <github@mafro.net>
2014-04-01 08:31:31 +1100

    removed symbol from weather segment: just show temp

--- segments/weather.sh
+++ segments/weather.sh
@@ -101,10 +101,6 @@ __yahoo_weather() {
 			# <yweather:condition  text="Clear"  code="31"  temp="66"  date="Mon, 01 Oct 2012 8:00 pm CST" />
 			degree=$(echo "$condition" | sed 's/.*temp="\([^"]*\)".*/\1/')
 			condition=$(echo "$condition" | sed 's/.*text="\([^"]*\)".*/\1/')
-			# Pull the times for sunrise and sunset so we know when to change the day/night indicator
-			# <yweather:astronomy sunrise="6:56 am"   sunset="6:21 pm"/>
-			sunrise=$(date -d"$(echo "$weather_data" | "$gnugrep" "yweather:astronomy" | sed 's/^\(.*\)sunset.*/\1/' | sed 's/^.*sunrise="\(.*m\)".*/\1/')" +%H%M)
-			sunset=$(date -d"$(echo "$weather_data" | "$gnugrep" "yweather:astronomy" | sed 's/^.*sunset="\(.*m\)".*/\1/')" +%H%M)
 		elif [ -f "${tmp_file}" ]; then
 			__read_tmp_file
 		fi
@@ -114,63 +110,8 @@ __yahoo_weather() {
 		if [ "$TMUX_POWERLINE_SEG_WEATHER_UNIT" == "k" ]; then
 			degree=$(echo "${degree} + 273.15" | bc)
 		fi
-		condition_symbol=$(__get_condition_symbol "$condition" "$sunrise" "$sunset") 
-		echo "${condition_symbol} ${degree}°$(echo "$TMUX_POWERLINE_SEG_WEATHER_UNIT" | tr '[:lower:]' '[:upper:]')" | tee "${tmp_file}"
+		echo "${degree}°$(echo "$TMUX_POWERLINE_SEG_WEATHER_UNIT" | tr '[:lower:]' '[:upper:]')" | tee "${tmp_file}"
 	fi
-}
-
-# Get symbol for condition. Available conditions: http://developer.yahoo.com/weather/#codes
-__get_condition_symbol() {
-	local condition=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-    local sunrise="$2"
-    local sunset="$3"
-	case "$condition" in
-		"sunny" | "hot")
-			hourmin=$(date +%H%M)
-			if [ "$hourmin" -ge "$sunset" -o "$hourmin" -le "$sunrise" ]; then
-				#echo "☽"
-				echo "☾"
-			else
-				#echo "☀"
-				echo "☼"
-			fi
-			;;
-		"rain" | "mixed rain and snow" | "mixed rain and sleet" | "freezing drizzle" | "drizzle" | "light drizzle" | "freezing rain" | "showers" | "mixed rain and hail" | "scattered showers" | "isolated thundershowers" | "thundershowers" | "light rain with thunder" | "light rain" | "rain and snow")
-			#echo "☂"
-			echo "☔"
-			;;
-		"snow" | "mixed snow and sleet" | "snow flurries" | "light snow showers" | "blowing snow" | "sleet" | "hail" | "heavy snow" | "scattered snow showers" | "snow showers" | "light snow" | "snow/windy" | "snow grains" | "snow/fog")
-			#echo "☃"
-			echo "❅"
-			;;
-		"cloudy" | "mostly cloudy" | "partly cloudy" | "partly cloudy/windy")
-			echo "☁"
-			;;
-		"tornado" | "tropical storm" | "hurricane" | "severe thunderstorms" | "thunderstorms" | "isolated thunderstorms" | "scattered thunderstorms")
-			#echo "⚡"
-			echo "☈"
-			;;
-		"dust" | "foggy" | "fog" | "haze" | "smoky" | "blustery" | "mist")
-			#echo "♨"
-			#echo "﹌"
-			echo "〰"
-			;;
-		"windy" | "fair/windy")
-			#echo "⚐"
-			echo "⚑"
-			;;
-		"clear" | "fair" | "cold")
-			hourmin=$(date +%H%M)
-			if [ "$hourmin" -ge "$sunset" -o "$hourmin" -le "$sunrise" ]; then
-				echo "☾"
-			else
-				echo "〇"
-			fi
-			;;
-		*)
-			echo "?"
-			;;
-	esac
 }
 
 __read_tmp_file() {
